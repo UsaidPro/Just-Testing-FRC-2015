00b472d1868a70680490a74027b91bc8e585aafd
David <David@172.17.147.7>
2015-03-21 10:36:55 -0500

    Fixed your bracing and added .getRawAxis()

--- Just-Testing-FRC-2015/src/org/usfirst/frc/team5431/robot/Robot.java
+++ Just-Testing-FRC-2015/src/org/usfirst/frc/team5431/robot/Robot.java
@@ -1,10 +1,19 @@
-//Removing testing - usaidpro
+//David, keep track of which libaries you import - you imported .CANTalon three times before I fixed it . . .
 package org.usfirst.frc.team5431.robot;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
 import org.usfirst.frc.team5431.robot.commands.ExampleCommand;
 import org.usfirst.frc.team5431.robot.subsystems.ExampleSubsystem;
 
@@ -15,28 +24,57 @@ import org.usfirst.frc.team5431.robot.subsystems.ExampleSubsystem;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot {
+public class Robot extends IterativeRobot 
+{
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
-
+	Joystick joy;
+	Compressor com;
+	Solenoid sol;
+	Solenoid sol2;
     Command autonomousCommand;
+    CANTalon rightDriveMotor;
+    CANTalon leftDriveMotor;
+    CANTalon LiftMotor;
+    CANTalon RightFlywheel;
+    CANTalon LeftFlywheel;
+    RobotDrive Robots;
+    
 
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
+    public void robotInit() 
+    {
+    	joy = new Joystick(0);
+    	com = new Compressor();
+    	sol = new Solenoid(0);
+    	sol2 = new Solenoid(4);
 		oi = new OI();
+		rightDriveMotor = new CANTalon(2);
+		leftDriveMotor  = new CANTalon(3);
+		LiftMotor  = new CANTalon(4);
+		RightFlywheel  = new CANTalon(5);
+		LeftFlywheel  = new CANTalon(6);
+    	Robots = new RobotDrive(leftDriveMotor, rightDriveMotor);
+    	
+		
         // instantiate the command used for the autonomous period
         autonomousCommand = new ExampleCommand();
+        
+        
     }
+    
 	
-	public void disabledPeriodic() {
+	public void disabledPeriodic() 
+	{
 		Scheduler.getInstance().run();
 	}
 
-    public void autonomousInit() {
+    public void autonomousInit() 
+    {
         // schedule the autonomous command (example)
         if (autonomousCommand != null) autonomousCommand.start();
     }
@@ -44,37 +82,83 @@ public class Robot extends IterativeRobot {
     /**
      * This function is called periodically during autonomous
      */
-    public void autonomousPeriodic() {
+    public void autonomousPeriodic() 
+    {
         Scheduler.getInstance().run();
     }
 
-    public void teleopInit() {
+    public void teleopInit() 
+    {
+    	com.start();
+        if (autonomousCommand != null) autonomousCommand.cancel();
+        
+    	while(true)
+    	{
+	    	if(joy.getRawButton(1) == true && joy.getRawButton(2) == false)
+	    	{
+	    		sol.set(true);
+	    		sol2.set(false);
+	
+	    	}
+	    	if(joy.getRawButton(1) == false && joy.getRawButton(2) == false||joy.getRawButton(1) == true && joy.getRawButton(2) == true)
+	    	{
+	    		sol.set(false);
+	    		sol.set(false);
+	    	}
+	    	if(joy.getRawButton(2) == true && joy.getRawButton(1) == false)
+	    	{
+	    		sol.set(false);
+	    		sol2.set(true);
+	    	}
+	    	if(joy.getRawAxis(5) > .1) //Runs the right motor forward if Y-Axis of the right joystick is above 0.1
+	    	{
+	    		rightDriveMotor.set(1);//Makes Motor run full speed forward
+	    	}
+	    	if(joy.getRawAxis(2) > .1)//Runs the left motor forward if Y-Axis of the left joystick is above 0.1
+	    	{
+	    		leftDriveMotor.set(1);//Makes left Motor run full speed forward
+	    	}
+	    	if((joy.getRawAxis(5) >= -.1) || (joy.getRawAxis(5) <= .1) ) //If joystick is not moved, turn off rightmotor
+	    	{
+	    		rightDriveMotor.set(0);
+	    	}
+	    	if((joy.getRawAxis(2) >= -.1) || (joy.getRawAxis(2) <= .1) ) //If joystick is not moved, turn off leftmotor
+	    	{
+	    		leftDriveMotor.set(0);
+	    	}
+	    	Timer.delay(0.1);
+	    	sol.set(false);
+	    	sol2.set(false);
+    	}
 		// This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to 
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
+
     }
 
     /**
      * This function is called when the disabled button is hit.
      * You can use it to reset subsystems before shutting down.
      */
-    public void disabledInit(){
+    public void disabledInit()
+    {
 
     }
 
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
+    public void teleopPeriodic() 
+    {
         Scheduler.getInstance().run();
     }
     
     /**
      * This function is called periodically during test mode
      */
-    public void testPeriodic() {
+    public void testPeriodic() 
+    {
         LiveWindow.run();
     }
 }
